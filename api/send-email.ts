d185a1f99bc3141a26979a6d61eea552f6f1384e
Nathan Sherburn <nathansherburn@gmail.com>
2024-08-12 23:04:46 +1000

    Clean up code

--- api/send-email.ts
+++ api/send-email.ts
@@ -2,6 +2,7 @@ import { Resend } from "resend";
 
 export async function POST(req: Request) {
   const body = await req.json();
+
   if (!body.resendApiKey) {
     return new Response("Missing resendApiKey", { status: 400 });
   }
@@ -14,11 +15,13 @@ export async function POST(req: Request) {
   if (!body.email.subject) {
     return new Response("Missing email.subject", { status: 400 });
   }
+
   const resend = new Resend(body.resendApiKey);
   let res = await resend.emails.send({
     ...body.email,
     from: "QiMail <qimail@nathansherburn.com>",
   });
+
   return new Response(JSON.stringify(res), {
     headers: {
       "content-type": "application/json",
