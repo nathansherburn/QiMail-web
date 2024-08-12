05c9b1ec39eb365140db5b5dd9c791d24abf8bc8
Nathan Sherburn <nathansherburn@gmail.com>
2024-08-12 17:31:02 +1000

    Add guards to resend route

--- api/send-email.ts
+++ api/send-email.ts
@@ -2,6 +2,18 @@ import { Resend } from "resend";
 
 export async function POST(req: Request) {
   const body = await req.json();
+  if (!body.resendApiKey) {
+    return new Response("Missing resendApiKey", { status: 400 });
+  }
+  if (!body.email) {
+    return new Response("Missing email", { status: 400 });
+  }
+  if (!body.email.to) {
+    return new Response("Missing email.to", { status: 400 });
+  }
+  if (!body.email.subject) {
+    return new Response("Missing email.subject", { status: 400 });
+  }
   const resend = new Resend(body.resendApiKey);
   let res = await resend.emails.send({
     ...body.email,
